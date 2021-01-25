2df0dc54c094b2f7ef109a4fec2ce3fb146b1c3b
Ryan Remaly <remalyryan@gmail.com>
2021-01-25 14:04:47 -0500

    can put #! /bin/bash -x to debug or put set -x in the top of the program

--- sed.sh
+++ sed.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
-
+# can add -x above to run in debug mode
 echo "Enter filename to substitute using sed"
 read fileName
 
 # g stands for global in the below sed command
 if [[ -f "$fileName"  ]]
 then
-	sed 's/i/I/g' $fileName > newFile.txt
+	sed 's/linux/weiners/g' $fileName > newFile.txt
 else
 	echo "$fileName doesnt exist"
 fi
